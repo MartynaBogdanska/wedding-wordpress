20a0c4acdcd460a3c8ebfac6a8778926cba4f59b
Martyna Bogdanska <martynabogdanska@MacBook-Pro-Martyna.local>
2018-01-08 20:10:23 +1000

    updated sticky-menu script, fixed mobile navigation

--- wordpress/wp-content/themes/understrap/js/theme.js
+++ wordpress/wp-content/themes/understrap/js/theme.js
@@ -3926,22 +3926,23 @@ Object.defineProperty(exports, '__esModule', { value: true });
 })();
 
 jQuery(function() {
-  /* Selecting the navigation */
-  var $nav = jQuery("#wrapper-navbar");
-  /* Determing navigation Size */
-  var $navSize =$nav.outerHeight();
-  /* Detecting scroll event */
-  jQuery(window).scroll(function() {
+    /* Selecting the navigation */
+    var $nav = jQuery("#wrapper-navbar");
+    /* Determing navigation Size */
+    var $navSize =$nav.outerHeight();
+    /* Detecting scroll event */
+    jQuery(window).scroll(function() {
     /* Determing the position of the scroll */
     var $scrollPos = jQuery(window).scrollTop();
     /* If it's bigger thant the navigation size */
-    if ($scrollPos >= 200) {
-      /* Adding the class scrolled  */
-      $nav.addClass("scrolled");
-      /* If it's not, then */
-    } else {
-      /* Removing the class */
-      $nav.removeClass("scrolled");
-    }
-  });
+    if ($scrollPos >= $navSize) {
+        /* Adding the class scrolled  */
+        $nav.addClass("scrolled");
+        /* If it's not, then */
+        }
+        else {
+        /* Removing the class */
+        $nav.removeClass("scrolled");
+        }
+    });
 });
